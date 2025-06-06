16c61a967d054df5cc7e4e6bb85fd82d09d319df
ianfuhr <ianfuhr10@gmail.com>
2025-06-06 13:25:23 -0400

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,8 @@
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,md,mdx}"],
-  safelist: ['animate-fade-in'],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
-  extend: {
-    fontFamily: {
-      sans: ['Inter', 'ui-sans-serif', 'system-ui'],
-    },
-    colors: {
-      primary: "#1F8BFF",
-      darkBlue: "#0E2544",
-    },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: "0" },
-          "100%": { opacity: "1" },
-        },
-      },
-      animation: {
-        "fade-in": "fadeIn 1s ease-out forwards",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
