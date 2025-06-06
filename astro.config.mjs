fda64ff057d9fd0c96863e7e97ac61629ba9d88b
ianfuhr <ianfuhr10@gmail.com>
2025-06-06 13:25:08 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+
+export default defineConfig({
+  integrations: [tailwind()]
+})
